0aa87832afaea45094bcc1466e6ad6a0d586b2f4
Ivan Mykhailovskyi <onesunvan@gmail.com>
2015-04-14 16:43:31 +0300

    Display pizzas to browser

---
+++ src/main/java/com/controllers/PizzaController.java
@@ -0,0 +1,26 @@
+package com.controllers;
+
+import com.tess.services.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author ivan
+ */
+@Controller
+public class PizzaController {
+
+    @Autowired
+    private PizzaService pizzaService;
+    
+    @RequestMapping(value = "/pizzas", method = RequestMethod.GET)
+    @ResponseBody
+    public String getPizzas() {
+        return pizzaService.getAllPizzas().toString();
+    }
+
+}
---
+++ src/main/java/com/controllers/PizzaDeliveryController.java
@@ -0,0 +1,36 @@
+package com.controllers;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author ivan
+ */
+@Controller
+public class PizzaDeliveryController /*implements ApplicationContextAware*/ {
+    
+    @Autowired
+    private ApplicationContext context;
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET) 
+    @ResponseBody
+    public String enterToPizzaService() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : context.getBeanDefinitionNames()) {
+            sb.append(s).append(" ");
+        }
+        return sb.toString();
+    }
+
+//    @Override
+//    public void setApplicationContext(ApplicationContext ac) throws BeansException {
+//        context = ac;
+//    }
+}
